1d03033979f174145301c1723d93dbac49439ef1
maverik0019 <imaveri@gmail.com>
2026-03-05 21:13:25 -0300

    Chapter 10 ARPU

--- Chapter 10/exercises.sql
+++ Chapter 10/exercises.sql
@@ -413,3 +413,65 @@ print(f"p-value  = {p_value_two_sided:.6g}")
 
 # Rule of thumb at alpha=0.05:
 # if p_value_two_sided < 0.05 -> reject H0 (rates differ)
+
+--ARPU (Average Revenue Per User)-----
+
+WITH revenue_user as ( 
+  
+SELECT
+user_id,
+SUM(amount) AS total_revenue
+FROM game_purchases
+GROUP BY user_id
+)
+SELECT
+    date_trunc('month', u.created)::date AS cohort_month,
+    COUNT(*) AS enrolled, 
+    SUM(COALESCE(r.total_revenue, 0)) AS total_revenue,
+    ROUND(
+      SUM(COALESCE(r.total_revenue, 0))::numeric/COUNT(*), 2
+      ) AS ARPU
+
+FROM game_users u
+LEFT JOIN revenue_user r
+ON u.user_id = r.user_id
+GROUP BY 1
+ORDER BY 1;
+
+---Average Revenue Per Paying User---
+WITH revenue_user AS (
+  SELECT
+    user_id,
+    SUM(amount) AS total_revenue
+  FROM game_purchases
+  GROUP BY user_id
+)
+
+SELECT
+  date_trunc('month', u.created)::date AS cohort_month,
+
+  COUNT(*) AS enrolled,
+
+  COUNT(r.user_id) AS buyers,
+
+  SUM(COALESCE(r.total_revenue,0)) AS total_revenue,
+
+  ROUND(
+    SUM(COALESCE(r.total_revenue,0))::numeric / COUNT(*),
+    2
+  ) AS arpu,
+
+  ROUND(
+    SUM(COALESCE(r.total_revenue,0))::numeric / NULLIF(COUNT(r.user_id),0),
+    2
+  ) AS arppu
+
+FROM game_users u
+
+LEFT JOIN revenue_user r
+ON u.user_id = r.user_id
+
+GROUP BY 1
+ORDER BY 1;
+
+--(80/20 revenue)
